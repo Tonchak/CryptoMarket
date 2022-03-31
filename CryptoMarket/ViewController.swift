b1ecf3b6a5ddf4763db163ff8a83a3d9ead588c3
Vitaliy Tonchak <vitaliy.tonchak@pal-solutions.dk>
2022-03-31 12:51:30 +0200

    Initial Commit

---
+++ CryptoMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoMarket
+//
+//  Created by Vitaliy Tonchak on 31/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
