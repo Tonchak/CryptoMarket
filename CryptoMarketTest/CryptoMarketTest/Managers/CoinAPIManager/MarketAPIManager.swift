f6d03784560e807e3e01baa7d8ea710dfd836ae7
Tonchak <tonchakvi@gmail.com>
2022-04-19 16:41:18 +0200

    fixed range of version for Alamofire,

--- CryptoMarketTest/CryptoMarketTest/Managers/CoinAPIManager/MarketAPIManager.swift
+++ CryptoMarketTest/CryptoMarketTest/Managers/CoinAPIManager/MarketAPIManager.swift
@@ -9,7 +9,12 @@ import UIKit
 import Alamofire
 
 class MarketAPIManager {
+    
+    private var afManager: Session = Alamofire.Session(configuration: URLSessionConfiguration.default)
+    private let rootLink: String = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
+    // MARK: - Initialization
+    
     static let shared: MarketAPIManager = {
         let instance = MarketAPIManager()
         
@@ -18,8 +23,7 @@ class MarketAPIManager {
     
     private init() {}
     
-    private var afManager: Session = Alamofire.Session(configuration: URLSessionConfiguration.default)
-    private let rootLink: String = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    // MARK: -
     
     func fetchList(completionHandler: @escaping (_ items: Array<ListingLatest>) -> Void) {
         
@@ -32,10 +36,6 @@ class MarketAPIManager {
             guard let allCurrencies = response.value else { return }
             completionHandler(allCurrencies.data!)
         }
-    }
-    
-    func fetchCurrenciesList() -> Array<Any> {
-        return Array.init()
     }
     
 }
